97c87cc4828465958b5fb2ac11c8227ad1376607
SergioRios.. <sergioarios30@gmail.com>
2026-09-25 08:07:13 -0500

    Add files via upload

---
+++ Segundo_Corte/Ejercicios/ejercicio 25 septiembre teoria/complejidad.py
@@ -0,0 +1,11 @@
+# realizar una secuencia de pasos un algoritmo
+# ¿que es n en esa secuencia
+#2 ¿cuantas veces se ejecuta el proceso si se duplican los datos?
+#3 Dentro de esa secuencia se invoca otro proceso cuantos procsos se ejecutan
+#4 que tipo de estructura de datos se utilizan
+
+# n representa el tamaño de la entrada del algoritmo lo podemos indicar como la cantidad de personas que participan en el proceso para las monitorias de nuestro proyecvto ya sean estudiante so monitores
+# depende de su complejidad seria necesario analizar cuantas operaciones se realizan en función de n. pero en la de nosotros utlizamos O(n^^2)
+# conun for podemos iterar sobre cada elemento de la entrada y realizar operaciones en función de n.
+# los arreglospara ests personas o un alistaa para la busqueda e insercion a la cantidad de personas y sus datos personlaes
+
